9e15b3dea1facfac9ad6d8e69e176ba209137e47
Ho Tuan Duong <zamakkat@gmail.com>
2016-06-19 23:39:32 +0800

    Add default value to tweets counter. Add code to generate ref code

--- db/migrate/20160619133321_create_customers.rb
+++ db/migrate/20160619133321_create_customers.rb
@@ -3,7 +3,7 @@ class CreateCustomers < ActiveRecord::Migration
     create_table :customers do |t|
       t.string :name
       t.string :code
-      t.integer :tweets_count
+      t.integer :tweets_count, default: 0
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 20160619133437) do
   create_table "customers", force: :cascade do |t|
     t.string   "name"
     t.string   "code"
-    t.integer  "tweets_count"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "tweets_count", default: 0
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "tweets", force: :cascade do |t|
