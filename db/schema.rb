030c80c0972901b4b427014e9ded0d1cc30eb32f
Ho Tuan Duong <zamakkat@gmail.com>
2016-06-25 16:48:43 +0800

    Remove redundant twitter info in customer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620050149) do
+ActiveRecord::Schema.define(version: 20160625084547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,9 @@ ActiveRecord::Schema.define(version: 20160620050149) do
   create_table "customers", force: :cascade do |t|
     t.string   "name"
     t.string   "code"
-    t.integer  "tweets_count",        default: 0
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "twitter_user_id"
-    t.string   "twitter_user_handle"
+    t.integer  "tweets_count", default: 0
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "tweets", force: :cascade do |t|
